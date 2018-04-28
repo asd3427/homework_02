5eefdad7f767b2eb75e74b4b45eb29f8f921b1f8
asd3427 <34855528+asd3427@users.noreply.github.com>
2018-04-28 12:48:43 +0800

    完成畫圖的要求

--- isir_Optdigits.py
+++ isir_Optdigits.py
@@ -5,54 +5,55 @@ from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.manifold import LocallyLinearEmbedding
 import pandas as pd
-
+import numpy as np
 
 iris = datasets.load_iris()
 digits = datasets.load_digits()
 wine = datasets.load_wine()
 
-#X = iris.data
-#y = iris.target
-#target_names = iris.target_names
-#
-#pca = PCA(n_components=2)
-#X_r = pca.fit(X).transform(X)
-#
-#lda = LinearDiscriminantAnalysis(n_components=2)
-#X_r2 = lda.fit(X, y).transform(X)
-#
-#lle =LocallyLinearEmbedding(n_neighbors=2)
-#X_r3 = lle.fit(X, y).transform(X)
-#
-## Percentage of variance explained for each components
-#print('explained variance ratio (first two components): %s'
-#      % str(pca.explained_variance_ratio_))
-#
-#plt.figure()
-#colors = ['red', 'turquoise', 'darkorange']
-#lw = 2
-#
-#for color, i, target_name in zip(colors, [0, 1, 2], target_names):
-#    plt.scatter(X_r[y == i, 0], X_r[y == i, 1], color=color, alpha=.8, lw=lw,
-#                label=target_name)
-#plt.legend(loc='best', shadow=False, scatterpoints=1)
-#plt.title('PCA of IRIS dataset')
-#
-#plt.figure()
-#for color, i, target_name in zip(colors, [0, 1, 2], target_names):
-#    plt.scatter(X_r2[y == i, 0], X_r2[y == i, 1], alpha=.8, color=color,
-#                label=target_name)
-#plt.legend(loc='best', shadow=False, scatterpoints=1)
-#plt.title('LDA of IRIS dataset')
-#plt.figure()
-#
-#for color, i, target_name in zip(colors, [0, 1, 2], target_names):
-#    plt.scatter(X_r3[y == i, 0], X_r3[y == i, 1], alpha=.8, color=color,
-#                label=target_name)
-#plt.legend(loc='best', shadow=False, scatterpoints=1)
-#plt.title('LLE of IRIS dataset')
-#
-#plt.show()
+X = iris.data
+y = iris.target
+target_names = iris.target_names
+
+pca = PCA(n_components=2)
+X_r = pca.fit(X).transform(X)
+
+lda = LinearDiscriminantAnalysis(n_components=2)
+X_r2 = lda.fit(X, y).transform(X)
+
+lle =LocallyLinearEmbedding(n_neighbors=2)
+X_r3 = lle.fit(X, y).transform(X)
+
+# Percentage of variance explained for each components
+print('explained variance ratio (first two components): %s'
+      % str(pca.explained_variance_ratio_))
+
+plt.figure()
+point = ['y*','r+', 'c>']
+lw=2
+
+for point, i, target_name in zip(point, [0, 1, 2], target_names):
+    plt.plot(X_r[y == i, 0], X_r[y == i, 1], point, alpha=1,
+                label=target_name)
+plt.legend(loc='best', shadow=False, scatterpoints=1)
+plt.title('PCA of IRIS dataset')
+
+plt.figure()
+point2= ['y*','r+', 'c>']
+for point2, i, target_name in zip(point2, [0, 1, 2], target_names):
+    plt.plot(X_r2[y == i, 0], X_r2[y == i, 1], point2,alpha=.8,lw=lw,
+                label=target_name)
+plt.legend(loc='best', shadow=False, scatterpoints=1)
+plt.title('LDA of IRIS dataset')
+plt.figure()
+point3= ['y*','r+', 'c>']
+for point3, i, target_name in zip(point3, [0, 1, 2], target_names):
+    plt.plot(X_r3[y == i, 0], X_r3[y == i, 1],point3, alpha=.8,lw=lw,
+                label=target_name)
+plt.legend(loc='best', shadow=False, scatterpoints=1)
+plt.title('LLE of IRIS dataset')
+
+plt.show()
 #這裡是 digits
 #X = digits.data
 #y = digits.target
@@ -99,49 +100,49 @@ wine = datasets.load_wine()
 #plt.show()
 
 #這裡是 sklearn datasets 內的 wine datasets
- 
-wine =datasets.load_wine()
-X = wine.data
-y = wine.target
-target_names = wine.target_names
-
-
-pca = PCA(n_components=2)
-X_r = pca.fit(X).transform(X)
-
-lda = LinearDiscriminantAnalysis(n_components=2)
-X_r2 = lda.fit(X, y).transform(X)
-
-lle =LocallyLinearEmbedding(n_neighbors=50)
-X_r3 = lle.fit(X, y).transform(X)
-
-# Percentage of variance explained for each components
-print('explained variance ratio (first two components): %s'
-      % str(pca.explained_variance_ratio_))
-
-plt.figure()
-colors = ['red', 'turquoise', 'darkorange']
-lw = 2
-
-for color, i, target_name in zip(colors, [0, 1, 2], target_names):
-    plt.scatter(X_r[y == i, 0], X_r[y == i, 1], color=color, alpha=.8, lw=lw,
-                label=target_name)
-plt.legend(loc='best', shadow=False, scatterpoints=1)
-plt.title('PCA of IRIS dataset')
-
-plt.figure()
-for color, i, target_name in zip(colors, [0, 1, 2], target_names):
-    plt.scatter(X_r2[y == i, 0], X_r2[y == i, 1], alpha=.8, color=color,
-                label=target_name)
-plt.legend(loc='best', shadow=False, scatterpoints=1)
-plt.title('LDA of IRIS dataset')
-plt.figure()
-
-for color, i, target_name in zip(colors, [0, 1, 2], target_names):
-    plt.scatter(X_r3[y == i, 0], X_r3[y == i, 1], alpha=.8, color=color,
-                label=target_name)
-plt.legend(loc='best', shadow=False, scatterpoints=1)
-plt.title('LLE of IRIS dataset')
-
-plt.show()
+# 
+#wine =datasets.load_wine()
+#X = wine.data
+#y = wine.target
+#target_names = wine.target_names
+#
+#
+#pca = PCA(n_components=2)
+#X_r = pca.fit(X).transform(X)
+#
+#lda = LinearDiscriminantAnalysis(n_components=2)
+#X_r2 = lda.fit(X, y).transform(X)
+#
+#lle =LocallyLinearEmbedding(n_neighbors=50)
+#X_r3 = lle.fit(X, y).transform(X)
+#
+## Percentage of variance explained for each components
+#print('explained variance ratio (first two components): %s'
+#      % str(pca.explained_variance_ratio_))
+#
+#plt.figure()
+#colors = ['red', 'turquoise', 'darkorange']
+#lw = 2
+#
+#for color, i, target_name in zip(colors, [0, 1, 2], target_names):
+#    plt.scatter(X_r[y == i, 0], X_r[y == i, 1], color=color, alpha=.8, lw=lw,
+#                label=target_name)
+#plt.legend(loc='best', shadow=False, scatterpoints=1)
+#plt.title('PCA of IRIS dataset')
+#
+#plt.figure()
+#for color, i, target_name in zip(colors, [0, 1, 2], target_names):
+#    plt.scatter(X_r2[y == i, 0], X_r2[y == i, 1], alpha=.8, color=color,
+#                label=target_name)
+#plt.legend(loc='best', shadow=False, scatterpoints=1)
+#plt.title('LDA of IRIS dataset')
+#plt.figure()
+#
+#for color, i, target_name in zip(colors, [0, 1, 2], target_names):
+#    plt.scatter(X_r3[y == i, 0], X_r3[y == i, 1], alpha=.8, color=color,
+#                label=target_name)
+#plt.legend(loc='best', shadow=False, scatterpoints=1)
+#plt.title('LLE of IRIS dataset')
+#
+#plt.show()
 
