63b505655ec85145ae8287458651ea0c86702320
asd3427 <34855528+asd3427@users.noreply.github.com>
2018-04-30 21:45:29 +0800

    最後更新
    以找出lle每次畫圖不同的問題點在哪裡
     lle method 有四種 每種的圖都不一樣 有的會取隨機數 所以圖繪不一樣 如下所顯示 這裡使用標準standard,modified,hessian,ltsa

--- Homework_02.py
+++ Homework_02.py
@@ -20,12 +20,16 @@ def iris():
     lda = LinearDiscriminantAnalysis(n_components=2)
     X_r2 = lda.fit(X, y).transform(X)
 
-    lle =LocallyLinearEmbedding(n_neighbors=2)
+    lle =LocallyLinearEmbedding(n_neighbors, n_components=2,
+                                      method='standard')
     X_r3 = lle.fit(X, y).transform(X)
 
 # Percentage of variance explained for each components
     print('explained variance ratio (first two components): %s'
       % str(pca.explained_variance_ratio_))
+    print('explained variance ratio (first two components): %s'
+      % str(lda.explained_variance_ratio_))
+   
 
     plt.figure()
     point = ['y*','r+', 'c>']
@@ -67,40 +71,46 @@ def digits():
     lda = LinearDiscriminantAnalysis(n_components=2)
     X_r2 = lda.fit(X, y).transform(X)
 
-    lle =LocallyLinearEmbedding(n_neighbors=2)
-    X_r3 = lle.fit(X, y).transform(X)
+    lle =LocallyLinearEmbedding(n_neighbors, n_components=2,
+                                      method='standard') # method 有四種 每種的圖都不一樣 有的會取隨機數 所以圖繪不一樣 如下所顯示 這裡使用標準
+                                                     # standard,modified,hessian,ltsa
+    X_lle = lle.fit(X,y).transform(X)
 
 # Percentage of variance explained for each components
     print('explained variance ratio (first two components): %s'
       % str(pca.explained_variance_ratio_))
 
     plt.figure()
-    point = ['y*','r+', 'c>','<','^','o','x','.','d']
+#----------------------------------pca------------------------------------------------
+
+    point = ['y*','r+', 'c>','<','^','o','x','.','d','s']
     lw=2
-
-
-    for point, i, target_name in zip(point,  [0, 1, 2,3,4,5,6,7,8,9], target_names):
+    for point, i, target_name in zip(point,  range(0,11), target_names):
         plt.plot(X_r[y == i, 0], X_r[y == i, 1], point, alpha=1,
                 label=target_name)
     plt.legend(loc='best', shadow=False, scatterpoints=1)
     plt.title('PCA of Digits dataset')
 
     plt.figure()
-    point2= ['y*','r+', 'c>','<','^','o','x','.','d']
-    for point2, i, target_name in zip(point2, [0, 1, 2,3,4,5,6,7,8,9], target_names):
+#----------------------------------lda------------------------------------------------
+
+    point2= ['y*','r+', 'c>','<','^','o','x','.','d','s']
+    for point2, i, target_name in zip(point2, range(0,11), target_names):
         plt.plot(X_r2[y == i, 0], X_r2[y == i, 1], point2,alpha=.8,lw=lw,
                 label=target_name)
     plt.legend(loc='best', shadow=False, scatterpoints=1)
     plt.title('LDA of Digits dataset')
     plt.figure()
-    point3= ['y*','r+', 'c>','<','^','o','x','.','d']
-    for point3, i, target_name in zip(point3, [0, 1, 2,3,4,5,6,7,8,9], target_names):
-        plt.plot(X_r3[y == i, 0], X_r3[y == i, 1],point3, alpha=.8,lw=lw,
+#----------------------------------lle------------------------------------------------
+
+    point3=  ['y*','r+', 'c>','<','^','o','x','.','d','s']
+    for point3, i, target_name in zip(point3,range(0,11), target_names):
+        plt.plot(X_lle[y == i, 0], X_lle[y == i, 1],point3, alpha=.8,lw=lw,
                 label=target_name)
     plt.legend(loc='best', shadow=False, scatterpoints=1)
     plt.title('LLE of Digits dataset')
-
     plt.show()
+
 
 def wine():
     
@@ -115,7 +125,8 @@ def wine():
     lda = LinearDiscriminantAnalysis(n_components=2)
     X_r2 = lda.fit(X, y).transform(X)
 
-    lle =LocallyLinearEmbedding(n_neighbors=2)
+    lle =LocallyLinearEmbedding(n_neighbors, n_components=2,
+                                      method='standard')
     X_r3 = lle.fit(X, y).transform(X)
 
 # Percentage of variance explained for each components
@@ -153,4 +164,3 @@ iris()
 digits()
 wine()
 
-test = datasets.load_digits()
